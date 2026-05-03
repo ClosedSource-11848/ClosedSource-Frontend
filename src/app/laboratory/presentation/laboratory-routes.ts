7b8562e0a1803cc5cb5cfae5331f73b2e5e5263a
BJRM03 <ruizbilly320@gmail.com>
2026-05-03 03:24:29 -0500

    feat(laboratory): add laboratory routes.

--- src/app/laboratory/presentation/laboratory-routes.ts
+++ src/app/laboratory/presentation/laboratory-routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
+import { Layout } from '../../shared/presentation/components/layout/layout';
 
-const labProfile = () =>
-  import('./views/lab-profile/lab-profile').then((m) => m.LabProfile);
+const labProfile = () => import('./views/lab-profile/lab-profile').then((m) => m.LabProfile);
 
-const staffList = () =>
-  import('./views/staff-list/staff-list').then((m) => m.StaffList);
+const staffList = () => import('./views/staff-list/staff-list').then((m) => m.StaffList);
 
-const staffForm = () =>
-  import('./views/staff-form/staff-form').then((m) => m.StaffForm);
+const staffForm = () => import('./views/staff-form/staff-form').then((m) => m.StaffForm);
 
 const productCatalog = () =>
   import('./views/product-catalog/product-catalog').then((m) => m.ProductCatalog);
 
-const productForm = () =>
-  import('./views/product-form/product-form').then((m) => m.ProductForm);
+const productForm = () => import('./views/product-form/product-form').then((m) => m.ProductForm);
 
 const rawMaterialList = () =>
   import('./views/raw-material-list/raw-material-list').then((m) => m.RawMaterialList);
@@ -22,14 +19,20 @@ const rawMaterialForm = () =>
   import('./views/raw-material-form/raw-material-form').then((m) => m.RawMaterialForm);
 
 const laboratoryRoutes: Routes = [
-  { path: 'lab-profile', loadComponent: labProfile },
-  { path: 'staff-list', loadComponent: staffList },
-  { path: 'staff-form', loadComponent: staffForm },
-  { path: 'product-catalog', loadComponent: productCatalog },
-  { path: 'product-form', loadComponent: productForm },
-  { path: 'raw-material-list', loadComponent: rawMaterialList },
-  { path: 'raw-material-form', loadComponent: rawMaterialForm },
-  { path: '', redirectTo: 'lab-profile', pathMatch: 'full' },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: 'lab-profile', loadComponent: labProfile },
+      { path: 'staff-list', loadComponent: staffList },
+      { path: 'staff-form', loadComponent: staffForm },
+      { path: 'product-catalog', loadComponent: productCatalog },
+      { path: 'product-form', loadComponent: productForm },
+      { path: 'raw-material-list', loadComponent: rawMaterialList },
+      { path: 'raw-material-form', loadComponent: rawMaterialForm },
+      { path: '', redirectTo: 'lab-profile', pathMatch: 'full' },
+    ],
+  },
 ];
 
 export { laboratoryRoutes };
